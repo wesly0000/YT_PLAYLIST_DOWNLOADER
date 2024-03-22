e47f95e7663ebcfed82faa887470bfa3ecce8705
WeslyCosta <wesly.virus@gmail.com>
2024-03-22 20:20:07 +0600

    downloader file added

---
+++ YT_PLAYLIST_DOWNLOADER.py
@@ -0,0 +1,20 @@
+from pytube import Playlist
+import re
+
+def download_playlist(url, download_path):
+    playlist = Playlist(url)
+    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+    print(f"Downloading {len(playlist)} videos...")
+
+    for video in playlist.videos:
+        try:
+            video.streams.get_highest_resolution().download(download_path)
+            print(f"Downloaded: {video.title}")
+        except Exception as e:
+            print(f"Error downloading {video.title}: {e}")
+
+if __name__ == "__main__":
+    playlist_url = input("Enter the URL of the YouTube playlist: ")
+    download_path = input("Enter the download path: ")
+
+    download_playlist(playlist_url, download_path)
